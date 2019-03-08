c495d7119befcecf880d6a233680f49529b08b57
kapiltathod <ktathod@grepruby.io>
2019-03-08 15:37:42 +0530

    features added in the todoitem model

--- server/models/todoitem.js
+++ server/models/todoitem.js
@@ -1,10 +1,46 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const TodoItem = sequelize.define('TodoItem', {
-    content: DataTypes.STRING
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+    itemName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    comment: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
   }, {});
+
   TodoItem.associate = function(models) {
-    // associations can be defined here
+    TodoItem.belongsTo(models.Todo, {
+      foreignKey: 'todoId',
+      onDelete: 'CASCADE',
+    });
+
+    TodoItem.hasMany(models.Comment, {
+      foreignKey: 'todoItemId',
+    });
   };
   return TodoItem;
 };
