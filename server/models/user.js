4568e4c71ebd3dbee94bb119341ca72bad92ebea
kapiltathod <ktathod@grepruby.io>
2019-03-08 14:57:14 +0530

    changes in the user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,59 @@
-'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    title: DataTypes.STRING
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    gender: {
+      type: DataTypes.ENUM,
+      allowNull: false,
+    },
+
+    dateOfBirth: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
   }, {});
+
   User.associate = function(models) {
-    // associations can be defined here
+    User.hasMany(models.Todo, {
+      foreignKey: 'userId',
+    });
+
+    User.hasMany(models.Comment, {
+      foreignKey: 'userId',
+    });
   };
   return User;
 };
