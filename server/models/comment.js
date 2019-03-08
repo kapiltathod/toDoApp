079139df4dfbc3324d56ac220e4d0e35c39e1946
kapiltathod <ktathod@grepruby.io>
2019-03-08 15:48:13 +0530

    corrections in the comment model

--- server/models/comment.js
+++ server/models/comment.js
@@ -1,10 +1,37 @@
-'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Comment = sequelize.define('Comment', {
-    content: DataTypes.STRING
+     id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+     comment: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
   }, {});
+
+
   Comment.associate = function(models) {
-    // associations can be defined here
+    Comment.belongsTo(models.TodoItem, {
+      foreignKey: 'todoItemId',
+      onDelete: 'CASCADE',
+    });
+
+    Comment.belongsTo(models.User, {
+      foreignKey: 'userId',
+      onDelete: 'CASCADE',
+    });
   };
   return Comment;
 };
