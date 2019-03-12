1f5714c9f598572ea93c1cb701f24a0dffc07d4f
kapiltathod <ktathod@grepruby.io>
2019-03-12 21:35:11 +0530

    added controllers for login and signup

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,7 +1,9 @@
 const authentication = require('../models').User;
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 module.exports = {
-  create(req, res) {
+  signup(req, res) {
     console.log(req.body)
     return authentication
       .create({
@@ -10,11 +12,43 @@ module.exports = {
         gender: req.body.gender,
         dateOfBirth: req.body.dateOfBirth,
         email: req.body.email,
-        password: req.body.password
+        password: bcrypt.hashSync(req.body.password, 10, function (err, hash) {
+       })
       })
       .then(user => res.status(201).send(user))
       .catch(error => res.status(400).send(error));
   },
-};
+
+
+  login(req, res) {
+    console.log('=====login===')
+    authentication.findOne({where: {email:req.body.email}})
+    .then(user=>{
+      //console.log('====', user)
+      bcrypt.compare(req.body.password, user.dataValues.password, (err, authResponse) => {
+        if(err){
+          return res.status(201).json({
+            err
+          });
+        }
+        console.log(authResponse)
+        if (authResponse) {
+          return res.status(201).send({
+          message: 'auth successful.',
+        });
+        } else {
+          return res.status(201).json({
+            message: 'auth failed'
+          });
+        }
+
+      });
+    })
+    .catch(err=>{
+      res.status(400).json({message:'Invalid Password/email'});
+    })
+
+  }
+}
 
 
