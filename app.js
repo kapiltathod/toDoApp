d7e297b9ae707e7272d8afdb01f7763ca11403f8
kapiltathod <ktathod@grepruby.io>
2019-03-13 19:23:40 +0530

    defined proper routes for the api calls

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const logger = require('morgan');
 const bodyParser = require('body-parser');
 const authentication = require('./server/controllers/authentication');
 const Todo = require('./server/controllers/todos');
+const TodoItem = require('./server/controllers/todoItems');
+const Comment = require('./server/controllers/comment')
 // Set up the express app
 const app = express();
 
@@ -13,20 +15,33 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
 // Setup a default catch-all route that sends back a welcome message in JSON format.
 app.get('/', (req, res) => res.status(200).send({
   message: 'Welcome to toDo App.',
 }));
 
-app.post('/signup', authentication.signup);
-app.post('/login', authentication.login);
 
 app.get('/getUsers', async function(req, res) {
     const users = await User.findAll({raw: true});
     res.send(users)
 })
 
-app.post('/todos', Todo.createTodo);
+//routes for authentication
+app.post('/signup', authentication.signup);
+app.post('/login', authentication.login);
 
+// routes for todo
+app.post('/todos', Todo.createTodo);
+app.get('/todos/list', Todo.list);
+app.get('/todos/retrieve', Todo.retrieve);
+
+
+
+// routes for todoItems
+app.post('/todoItems', TodoItem.createTodoItems);
+
+//routes for comment
+app.post('/comments', Comment.createComments);
 
 module.exports = app;
