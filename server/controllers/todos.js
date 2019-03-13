3737680c93e6481de2782cc444ff5a332482457b
kapiltathod <ktathod@grepruby.io>
2019-03-13 19:18:56 +0530

    included the list and retrieve feature

--- server/controllers/todos.js
+++ server/controllers/todos.js
@@ -1,4 +1,5 @@
 const Todo = require('../models').Todo;
+const TodoItem = require('../models').TodoItem;
 
 module.exports = {
   createTodo (req, res) {
@@ -7,6 +8,33 @@ module.exports = {
         title: req.body.title,
       })
       .then(todo => res.status(201).send(todo))
+      .catch(error => res.status(400).send(error));
+  },
+
+  list(req, res) {
+    return Todo
+      .findAll({
+        include: [{
+          itemName: req.body.itemName,
+          description: req.body.description,
+          comment: req.body.comment
+        }],
+      })
+      .then(todos => res.status(200).send(todos))
+      .catch(error => res.status(400).send(error));
+  },
+
+  retrieve(req, res) {
+    return Todo
+      .findById(req.params.todoId)
+      .then(todo => {
+        if (!todo) {
+          return res.status(404).send({
+           message: 'Todo Not Found',
+          });
+        }
+        return res.status(200).send(todo);
+      })
       .catch(error => res.status(400).send(error));
   },
 };
