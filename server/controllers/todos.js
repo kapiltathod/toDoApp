f840aeb84684b5c8d877edd0f2ee2f62af167f98
kapiltathod <ktathod@grepruby.io>
2019-03-15 21:09:48 +0530

    converted object into class with async await

--- server/controllers/todos.js
+++ server/controllers/todos.js
@@ -1,25 +1,45 @@
 const Todo = require('../models').Todo;
-const TodoItem = require('../models').TodoItem;
+// const TodoItem = require('../models').TodoItem;
 
-module.exports = {
-  create (req, res) {
-    return Todo
-      .create({
-        title: req.body.title,
-      })
-      .then(todo => res.status(201).send(todo))
-      .catch(error => res.status(400).send(error));
-  },
+// module.exports = {
+//   create (req, res) {
+//     return Todo
+//       .create({
+//         title: req.body.title,
+//       })
+//       .then(todo => res.status(201).send(todo))
+//       .catch(error => res.status(400).send(error));
+//   },
 
-  list (req, res) {
-    return Todo
-      .findAll({
-        include: [{
-          model: TodoItem,
-          as: 'todoItems',
-        }],
-      })
-      .then(todos => res.status(200).send(todos))
-      .catch(error => res.status(400).send(error));
-  },
+//   list (req, res) {
+//     return Todo
+//       .findAll({
+//         include: [{
+//           model: TodoItem,
+//           as: 'todoItems',
+//         }],
+//       })
+//       .then(todos => res.status(200).send(todos))
+//       .catch(error => res.status(400).send(error));
+//   },
+// }
+
+
+
+class Todos {
+  static async create(res, req) {
+    try {
+      const todo = await Todo
+        .create ({
+          title: req.body.title,
+        })
+        res.status(201).send(todo)
+    } catch (error) {
+      console.log("okay now we are here");
+      return res.status(400).send(error);
+    }
+  }
 }
+
+
+module.exports = Todos;
