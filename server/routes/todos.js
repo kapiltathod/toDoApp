5cffa91ffa4e57a3f498521be7893b963f0c6d06
kapiltathod <ktathod@grepruby.io>
2019-03-15 21:10:17 +0530

    corrected the routes

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
-const Todo = require('../controllers/todos');
+const Todos = require('../controllers/todos');
 const checkAuth = require('../../middlewares/checkAuth');
 
-router.post('/', checkAuth, Todo.create);
-router.get('/', checkAuth, Todo.list);
+router.post('/', checkAuth, Todos.create);
 
 module.exports = router;
+
+
+
+// const Todo = require('../controllers/todos');
+// router.post('/', checkAuth, Todo.create);
+// router.get('/', checkAuth, Todo.list);
