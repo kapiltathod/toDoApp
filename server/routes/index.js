70c53d07b374a774eb1d03a1063eef4147b37707
kapiltathod <ktathod@grepruby.io>
2019-03-15 19:55:00 +0530

    corrected the routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
-const User = require('./authentication');
+const Authentication = require('./authentication');
 const Todo = require('./todos');
 const TodoItem = require('./todoItems');
 
-router.use('/authentication', User);
+router.use('/authentication', Authentication);
 router.use('/todos', Todo);
-router.use('/todos/:todoId/items', TodoItem);
+router.use('/todos/:todoId/todoItems', TodoItem);
 
 module.exports = router;
