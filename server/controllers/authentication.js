2bd37005a55cbc9f814c9cf33fd4b806f7ddb88a
kapiltathod <ktathod@grepruby.io>
2019-03-15 19:56:15 +0530

    included classes with async awit

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -2,36 +2,33 @@ const User = require('../models').User;
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+class Authentication {
+  static async signup(req, res) {
+    try {
+      const user = await User
+        .create({
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          gender: req.body.gender,
+          dateOfBirth: req.body.dateOfBirth,
+          email: req.body.email,
+          password: bcrypt.hashSync(req.body.password, 10, function (err, hash) {
+          })
+        })
+        res.status(201).send(user)
+    } catch (error) {
+      return res.status(400).send(error);
+    }
+  }
 
-
-module.exports = {
-  signup(req, res) {
-   return User
-      .create({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        gender: req.body.gender,
-        dateOfBirth: req.body.dateOfBirth,
-        email: req.body.email,
-        password: bcrypt.hashSync(req.body.password, 10, function (err, hash) {
-       })
-      })
-      .then(user => res.status(201).send(user))
-      .catch(error => res.status(400).send(error));
-  },
-
-  login(req, res) {
-    console.log(req.body);
-    User.findOne({where: {email:req.body.email}})
-    .then(user => {
-      console.log('====', user)
-      bcrypt.compare(req.body.password, user.dataValues.password, (err, authResponse) => {
-        if(err){
-          return res.status(201).json({
-            err
-          });
+  static async login(req, res) {
+    try {
+      const user = await User
+        .findOne({where: {email:req.body.email}})
+        bcrypt.compare(req.body.password, user.dataValues.password, (err, authResponse) => {
+        if(err) {
+          return res.status(201).json({err});
         }
-        console.log(authResponse)
         if (authResponse) {
           const token = jwt.sign ({
             email: user.dataValues.email,
@@ -51,11 +48,61 @@ module.exports = {
           });
         }
       });
-    })
-    .catch(err=>{
-      res.status(400).json({message:'Go for signup'});
-    })
+    } catch (error) {
+      return res.status(400).send(error);
+    }
   }
 }
 
+module.exports = Authentication;
+
+
+// module.exports = {
+
+//   signup(req, res) {
+//     return User
+//       .create({
+//         firstName: req.body.firstName,
+//         lastName: req.body.lastName,
+//         gender: req.body.gender,
+//         dateOfBirth: req.body.dateOfBirth,
+//         email: req.body.email,
+//         password: bcrypt.hashSync(req.body.password, 10, function (err, hash) {
+//        })
+//       })
+//       .then(user => res.status(201).send(user))
+//       .catch(error => res.status(400).send(error));
+//   },
+
+//   login(req, res) {
+//     User.findOne({where: {email:req.body.email}})
+//     .then(user => {
+//       bcrypt.compare(req.body.password, user.dataValues.password, (err, authResponse) => {
+//         if(err) {
+//           return res.status(201).json({err});
+//         }
+//         if (authResponse) {
+//           const token = jwt.sign ({
+//             email: user.dataValues.email,
+//             id: user.dataValues.id
+//           },
+//             process.env.JWT_KEY, {
+//             expiresIn: "1h"
+//             }
+//           )
+//           return res.status(201).send({
+//           message: 'User successful.',
+//           token: token
+//           });
+//         } else {
+//           return res.status(201).json({
+//           message: 'Incorrect email/password'
+//           });
+//         }
+//       });
+//     })
+//     .catch(err=>{res.status(400).json({message:'Go for signup'});
+//     })
+//   }
+// }
 
