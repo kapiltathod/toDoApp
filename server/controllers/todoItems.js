ce2207aa47e86e6647c47ae0e9eb2b21a214574c
kapiltathod <ktathod@grepruby.io>
2019-03-15 21:11:04 +0530

    included clas with async await

--- server/controllers/todoItems.js
+++ server/controllers/todoItems.js
@@ -1,22 +1,40 @@
 const TodoItem = require('../models').TodoItem;
 
-module.exports = {
+// module.exports = {
 
-   create(req, res) {
-    return TodoItem
-      .create({
+//    create(req, res) {
+//     return TodoItem
+//       .create({
+//         itemName: req.body.itemName,
+//         description: req.body.description,
+//         comment: req.body.comment
+//       })
+//       .then(todoItem => res.status(201).send(todoItem))
+//       .catch(error => res.status(400).send(error));
+//   },
+
+//    list(req, res) {
+//     return TodoItem
+//       .all()
+//       .then(todoItems => res.status(200).send(todoItems))
+//       .catch(error => res.status(400).send(error));
+//     },
+// };
+
+class TodoItems {
+  static async create(req, res) {
+    try {
+      const todoItems = await TodoItem
+        .create({
         itemName: req.body.itemName,
         description: req.body.description,
         comment: req.body.comment
       })
-      .then(todoItem => res.status(201).send(todoItem))
-      .catch(error => res.status(400).send(error));
-  },
+      res.status(201).send(todoItem)
+    } catch (error) {
+      res.status(400).send(error)
+    }
+  }
+}
 
-   list(req, res) {
-    return TodoItem
-      .all()
-      .then(todoItems => res.status(200).send(todoItems))
-      .catch(error => res.status(400).send(error));
-    },
-};
+module.exports = TodoItems;
