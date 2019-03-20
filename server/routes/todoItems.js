8ab8f62be0e03302ba949260adb810a6775ef8e2
kapiltathod <ktathod@grepruby.io>
2019-03-20 11:42:03 +0530

    inserted validator middleware

--- server/routes/todoItems.js
+++ server/routes/todoItems.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const checkAuth = require('../middlewares/checkAuth');
 const TodoItems = require('../controllers/todoItems');
+const validatorMiddleware = require('../middlewares/validator');
+const todoItemSchema = require('../schemas/todoItemSchema');
 
-router.post('/todos/:todoId/todoItems', checkAuth, TodoItems.create);
+router.post('/todos/:todoId/todoItems', validatorMiddleware(todoItemSchema), TodoItems.create);
 router.get('/todos/:todoId/todoItems', checkAuth, TodoItems.list);
 
 module.exports = router;
