bafcaacb8fbd02acb33902f6d4d17482723e8f12
kapiltathod <ktathod@grepruby.io>
2019-03-20 11:41:02 +0530

    insertion of validator

--- server/controllers/todoItems.js
+++ server/controllers/todoItems.js
@@ -3,26 +3,14 @@ const TodoItem = require('../models').TodoItem;
 class TodoItems {
   static async create(req, res) {
     try {
-      console.log('====1')
       const todoItem = await TodoItem
         .create({
-        itemName: req.body.itemName,
-        description: req.body.description,
-        comment: req.body.comment,
-      })
-      if (req.body.itemName.length < 3 ) {
-        console.log('====2')
-        return res.status(201).send({
-        message: 'itemName length should be more than 3'
+          itemName: req.body.itemName,
+          description: req.body.description,
+          comment: req.body.comment,
+          todoId: req.params.todoId,
         })
-      }
-      if(/^[a-zA-Z0-9]*$/.test(req.body.title) == false) {
-        return res.status(201).send({
-        message: 'Not alpha numeric'
-        })
-      } else {
         res.status(201).send(todoItem)
-        }
     } catch (error) {
       res.status(400).send(error)
     }
