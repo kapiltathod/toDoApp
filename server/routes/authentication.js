88c89191f6180b19be0109571aac885f2d3cfdca
kapiltathod <ktathod@grepruby.io>
2019-03-20 11:37:39 +0530

    included the validator middleware

--- server/routes/authentication.js
+++ server/routes/authentication.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const Authentication = require('../controllers/authentication');
+const signupAuth = require('../middlewares/signupAuth');
+const validatorMiddleware = require('../middlewares/validator');
+const signupSchema = require('../schemas/signupSchema');
 
-router.post('/signup', Authentication.signup);
+router.post('/signup', validatorMiddleware(signupSchema), Authentication.signup);
 router.post('/login', Authentication.login);
 
 module.exports = router;
