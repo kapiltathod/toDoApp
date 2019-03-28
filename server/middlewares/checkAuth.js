d67ae9e45fc96417b0aa66f08b2cef180738e712
kapiltathod <ktathod@grepruby.io>
2019-03-28 17:40:23 +0530

    refactored the req header for token

--- server/middlewares/checkAuth.js
+++ server/middlewares/checkAuth.js
@@ -1,9 +1,11 @@
 const jwt = require('jsonwebtoken')
+const config = require('../config/config.json')
 
 module.exports = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(' ')[1]
-    const decoded = jwt.verify(token, process.env.JWT_KEY)
+    const token = req.headers.authorization
+    console.log(req.headers)
+    const decoded = jwt.verify(token, config[process.env.NODE_ENV].JWT_KEY)
     req.user = decoded
     next()
   } catch (error) {
